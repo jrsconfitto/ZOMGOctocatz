cbbe313673c70d0a4b8e39a96794c4106659c28f
James Sconfitto <james@exele.com>
2012-10-09 13:01:05 -0400

    YES

---
+++ ZOMGOctocatz/Atoms.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octodexing {
+	public class Atom {
+		public String Title { get; set; }
+		public String Link { get; set; }
+		public String Updated { get; set; }
+		public String ID { get; set; }
+		public Author Author { get; set; }
+		public List<Entry> Entries { get; set; }
+	}
+
+	public class Author {
+		public String Name { get; set; }
+		public String Email { get; set; }
+	}
+
+	public class Entry {
+		public String Title { get; set; }
+		public String Link { get; set; }
+		public String Updated { get; set; }
+		public String ID { get; set; }
+		public Content Content { get; set; }
+	}
+
+	public class Content {
+		public Div Div { get; set; }
+		public String Type { get; set; }
+	}
+
+	public class Div {
+		public A A { get; set; }
+	}
+
+	public class A {
+		public String HREF { get; set; }
+		public IMG IMG { get; set; }
+	}
+
+	public class IMG {
+		public String SRC { get; set; }
+	}
+}
---
+++ ZOMGOctocatz/Program.cs
@@ -0,0 +1,49 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Octodexing {
+	public class Program {
+		static void Main(string[] args) {
+			// Find the directory for the Octocats
+			var Pictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+			var OctocatsDirectory = Path.Combine(Pictures, "Octocatz");
+			if (!Directory.Exists(OctocatsDirectory)) {
+				Directory.CreateDirectory(OctocatsDirectory);
+			}
+
+			// This application will scrape all the Octodex cats and put them in a folder in your Pictures library
+			var client = new RestClient("http://feeds.feedburner.com/Octocats");
+			var request = new RestRequest(Method.GET);
+
+			try {
+				// async with deserialization
+				var asyncHandle = client.ExecuteAsync<Atom>(request, response => {
+					response.Data.Entries.ForEach(entry => {
+						var content = entry.Content;
+						var octocatLink = entry.Content.Div.A.IMG.SRC;
+						var octocatFilePath = Path.Combine(OctocatsDirectory, entry.Title + ".png");
+
+						if (!File.Exists(octocatFilePath)) {
+							var gimmeOCTOCATZ = new RestClient(octocatLink);
+							var octocatRequest = new RestRequest(Method.GET);
+
+							var anotherAsyncHandle = gimmeOCTOCATZ.ExecuteAsync(octocatRequest, octocat => {
+								Console.WriteLine("Saving {0} to {1}", entry.Title, octocatFilePath);
+								File.WriteAllBytes(octocatFilePath, octocat.RawBytes);
+							});
+						}
+					});
+				});
+			}
+			catch (Exception ex) {
+				var message = ex.Message;
+			}
+
+			Console.WriteLine("Press enter to end the application");
+			Console.WriteLine();
+			Console.ReadLine();
+		}
+	}
+}
