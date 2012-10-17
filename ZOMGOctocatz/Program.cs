450a10ef5cc5ea26208a44ec6b59bbca19d492f4
James Sconfitto <james@exele.com>
2012-10-17 14:20:49 -0400

    Give output directory on command line

--- ZOMGOctocatz/Program.cs
+++ ZOMGOctocatz/Program.cs
@@ -4,46 +4,59 @@ using System.Collections.Generic;
 using System.IO;
 
 namespace Octodexing {
-	public class Program {
-		static void Main(string[] args) {
-			// Find the directory for the Octocats
-			var Pictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-			var OctocatsDirectory = Path.Combine(Pictures, "Octocatz");
-			if (!Directory.Exists(OctocatsDirectory)) {
-				Directory.CreateDirectory(OctocatsDirectory);
-			}
+    public class Program {
+        static void Main(string[] args) {
+            String OctocatsDirectory = "";
 
-			// This application will scrape all the Octodex cats and put them in a folder in your Pictures library
-			var client = new RestClient("http://feeds.feedburner.com/Octocats");
-			var request = new RestRequest(Method.GET);
+            if (args.Length == 0) {
+                // Find the directory for the Octocats
+                var Pictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                OctocatsDirectory = Path.Combine(Pictures, "Octocatz");
+                if (!Directory.Exists(OctocatsDirectory)) {
+                    Directory.CreateDirectory(OctocatsDirectory);
+                }
+            }
+            else if (args.Length == 1) {
+                if (Directory.Exists(args[0])) {
+                    OctocatsDirectory = args[0];
+                }
+                else {
+                    Console.WriteLine("Please pass in a valid path for the octocatz");
+                    return;
+                }
+            }
 
-			try {
-				// async with deserialization
-				var asyncHandle = client.ExecuteAsync<Atom>(request, response => {
-					response.Data.Entries.ForEach(entry => {
-						var content = entry.Content;
-						var octocatLink = entry.Content.Div.A.IMG.SRC;
-						var octocatFilePath = Path.Combine(OctocatsDirectory, entry.Title + ".png");
+            // This application will scrape all the Octodex cats and put them in a folder in your Pictures library
+            var client = new RestClient("http://feeds.feedburner.com/Octocats");
+            var request = new RestRequest(Method.GET);
 
-						if (!File.Exists(octocatFilePath)) {
-							var gimmeOCTOCATZ = new RestClient(octocatLink);
-							var octocatRequest = new RestRequest(Method.GET);
+            try {
+                // async with deserialization
+                var asyncHandle = client.ExecuteAsync<Atom>(request, response => {
+                    response.Data.Entries.ForEach(entry => {
+                        var content = entry.Content;
+                        var octocatLink = entry.Content.Div.A.IMG.SRC;
+                        var octocatFilePath = Path.Combine(OctocatsDirectory, entry.Title + ".png");
 
-							var anotherAsyncHandle = gimmeOCTOCATZ.ExecuteAsync(octocatRequest, octocat => {
-								Console.WriteLine("Saving {0} to {1}", entry.Title, octocatFilePath);
-								File.WriteAllBytes(octocatFilePath, octocat.RawBytes);
-							});
-						}
-					});
-				});
-			}
-			catch (Exception ex) {
-				var message = ex.Message;
-			}
+                        if (!File.Exists(octocatFilePath)) {
+                            var gimmeOCTOCATZ = new RestClient(octocatLink);
+                            var octocatRequest = new RestRequest(Method.GET);
 
-			Console.WriteLine("Press enter to end the application");
-			Console.WriteLine();
-			Console.ReadLine();
-		}
-	}
+                            var anotherAsyncHandle = gimmeOCTOCATZ.ExecuteAsync(octocatRequest, octocat => {
+                                Console.WriteLine("Saving {0} to {1}", entry.Title, octocatFilePath);
+                                File.WriteAllBytes(octocatFilePath, octocat.RawBytes);
+                            });
+                        }
+                    });
+                });
+            }
+            catch (Exception ex) {
+                var message = ex.Message;
+            }
+
+            Console.WriteLine("Press enter to end the application");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
 }
